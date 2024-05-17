b51c2a41b2bf79ffc48a25743cabfc91b67a5470
Chase Swedlo <chasechaseswedlo@outlook.com>
2024-05-17 11:56:12 -0500

    Commit project

---
+++ src/pages/Home.js
@@ -0,0 +1,67 @@
+import { FaGithub } from 'react-icons/fa';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { motion } from 'framer-motion';
+
+function Home() {
+  const [isValid, setIsValid] = useState(true);
+  const token = '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';
+  const options = { headers: { Authorization: `Bearer ${token}` } };
+  const navigate = useNavigate();
+
+  const fetchUser = async (userInput) => {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${userInput}`, options);
+      console.log('API response data:', response.data);
+      setIsValid(true);
+      navigate(`/user/${userInput}`);
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        console.log('User not found, setting isValid to false');
+        setIsValid(false);
+      }
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchUser(event.target.userinput.value);
+    event.target.userinput.value = '';
+  };
+
+  return (
+    <motion.div
+      key='home'
+      className='home'
+      initial={{ opacity: 0, y: -25 }}
+      animate={{ 
+        opacity: 1, 
+        y: 0,
+        transition: { duration: 1 }
+      }}
+      exit={{ 
+        opacity: 0, 
+        y: -25,
+        transition: { duration: 0.4 }
+      }}
+    >
+      <div className='search'>
+        <FaGithub />
+        <div className='text-border'>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              placeholder='Username'
+              name='userinput'
+              autoComplete='off'
+            />
+          </form>
+        </div>
+        <p>{isValid ? 'Welcome to GitHub Finder' : 'Not a valid user'}</p>
+      </div>
+    </motion.div>
+  );
+}
+
+export default Home;
---
+++ src/pages/User.js
@@ -0,0 +1,105 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import axios from 'axios';
+
+function User() {
+  const { slug } = useParams();
+  const [user, setUser] = useState([]);
+  const [repos, setRepos] = useState([]);
+  const token = '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';
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const options = {headers: { Authorization: `Bearer ${token}`}};
+    const fetchUser = async (slug) => {
+      try {
+        const [userResponse, reposResponse] = await Promise.all([
+          axios.get(`https://api.github.com/users/${slug}`, options),
+          axios.get(`https://api.github.com/users/${slug}/repos`, options)
+        ]);
+        console.log('API response data:', reposResponse.data);
+        setUser(userResponse.data);
+        setRepos(reposResponse.data);
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          console.log('User not found');
+        }
+        navigate(`/`);
+      }
+    }
+    fetchUser(slug);
+  }, [slug, navigate]);
+
+
+  const formatDate = (date) => {
+    const options = {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric'
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
+}
+
+  return (
+    <motion.div 
+      key='user'
+      initial={{ opacity: 0, y: -25 }}
+      animate={{ 
+        opacity: 1, 
+        y: 0,
+        transition: { duration: 0.4, delay: 0.85 }
+      }}
+      exit={{ 
+        opacity: 0, 
+        y: -25,
+        transition: { duration: 0.4 }
+      }}
+    >
+      {user ? (
+        <div className='user-info'>
+          <figure>
+            <img alt={user.avatar_url} src={user.avatar_url}/>
+          </figure>
+          <h1>{user.name === null ? user.login : user.name}</h1>
+          <div className='stats flex'>
+            <p><span>{user.public_repos}</span>Repositories</p>
+            <p><span>{user.followers}</span>Followers</p>
+            <p><span>{user.following}</span>Following</p>
+          </div>
+          <a href={user.html_url} target="_blank" rel="noreferrer">
+            <input type="button" value="Go to Github"/>
+          </a>
+        </div>
+      ) : (
+        <p>User data not found</p>
+      )}
+      <section className='repos'>
+      <h2>My repositories</h2>
+      {repos ? (
+        repos.length > 0 ? (
+          repos.map(repo => (
+            <div className='repo' key={repo.id}>
+              <div className='flex sb'>
+                <a href={repo.html_url} target="_blank" rel="noreferrer">
+                  <h3>{repo.name}</h3>
+                </a>
+                <span className='date'>{formatDate(repo.updated_at)}</span>
+              </div>
+              {repo.description !== null && <p>{repo.description}</p>}
+            </div>
+          ))
+        ) : (
+        <div className='repo'>
+          <h3>No repositories</h3>
+        </div>)
+        ) : (
+        <p>User data not found</p>
+      )
+      }
+      </section>
+    </motion.div>
+  );
+}
+
+export default User;
